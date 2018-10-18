8c64000d240665e5ea53cbc76ce5cdac6929cc32
Zepse wolf <douglasmeows@gmail.com>
2018-10-18 14:25:02 +0700

    remake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ApprovalComponent } from './approval/approval.component';
+import { CompanyFeedComponent } from './company-feed/company-feed.component';
+import { EmployeeFeedComponent } from './employee-feed/employee-feed.component';
+import { RouterModule, Routes} from '@angular/router';
+const routes: Routes=[
+  {path: 'company', component: CompanyFeedComponent},
+  {path: 'employee', component: EmployeeFeedComponent},
+  {path: '', redirectTo: 'company', pathMatch:'full'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApprovalComponent,
+    CompanyFeedComponent,
+    EmployeeFeedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
